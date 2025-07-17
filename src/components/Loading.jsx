275193e1a48a36e3160cb956f1bd6feef54e9b8d
dhameliyahit <heetdhameliya59@gmail.com>
2025-07-17 21:31:51 +0530

    auth final touch

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/70 bg-opacity-50 z-50">
+            <span className="loading loading-bars loading-xl text-white"></span>
+        </div>
+    )
+}
+
+export default Loading
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -7,6 +7,7 @@ import { FaEye } from "react-icons/fa";
 import { toast } from 'react-toastify';
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import Loading from '../Loading';
 
 
 const API_URL = "http://localhost:5000"
@@ -16,16 +17,18 @@ export default function Login() {
 
     const { handleSubmit, register } = useForm();
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false)
 
     const togglePassword = () => setShowPassword(!showPassword);
 
     const handleLogin = async (data) => {
         try {
+            setLoading(true)
             const res = await axios.post(`${API_URL}/api/auth/login`, {
                 email: data.email,
                 password: data.password
             });
-
+            setLoading(false)
             const { token, user } = res.data;
 
             localStorage.setItem("Authorization", `Bearer ${token}`);
@@ -46,70 +49,75 @@ export default function Login() {
 
         } catch (error) {
             toast.error(error.response?.data?.message || "Login Failed");
+            setLoading(false)
         }
     };
 
 
     return (
-        <Layout>
-            <div className="min-h-screen flex flex-col items-center justify-center sm:p-4">
-                <div className="w-full max-w-md bg-white shadow-xl rounded-lg sm:py-8 sm:px-8 py-6 px-4">
-                    {/* Logo */}
-                    <div className="text-center mb-6">
-                        <img
-                            src="assets/ShreeXpressLogo.png"
-                            alt="ShreeXpress Courier Service"
-                            className="h-16 mx-auto object-contain"
-                        />
-                        <h2 className="sm:text-2xl text-xl font-bold text-gray-700 mt-2">Welcome Back</h2>
-                        <p className="sm:text-sm text-xs text-gray-500">Login to ShreeXpress Courier Service</p>
-                    </div>
+        <>
+            {loading && <Loading />}
 
-                    {/* Form */}
-                    <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
-                        <div>
-                            <label className="block text-sm font-medium text-gray-600">Email</label>
-                            <input
-                                type="email"
-                                {...register("email")}
-                                placeholder="you@example.com"
-                                className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-[#383185] border-gray-300"
-                                required
+            <Layout>
+                <div className="min-h-screen flex flex-col items-center justify-center sm:p-4">
+                    <div className="w-full max-w-md bg-white shadow-xl rounded-lg sm:py-8 sm:px-8 py-6 px-4">
+                        {/* Logo */}
+                        <div className="text-center mb-6">
+                            <img
+                                src="assets/ShreeXpressLogo.png"
+                                alt="ShreeXpress Courier Service"
+                                className="h-16 mx-auto object-contain"
                             />
+                            <h2 className="sm:text-2xl text-xl font-bold text-gray-700 mt-2">Welcome Back</h2>
+                            <p className="sm:text-sm text-xs text-gray-500 ">Login to ShreeXpress Courier Service</p>
                         </div>
-                        <div>
-                            <label className="block text-sm font-medium text-gray-600">Password</label>
-                            <div className='relative'>
+
+                        {/* Form */}
+                        <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
+                            <div>
+                                <label className="block text-sm font-medium text-gray-600">Email</label>
                                 <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    {...register("password")}
-                                    placeholder="••••••••"
+                                    type="email"
+                                    {...register("email")}
+                                    placeholder="you@example.com"
                                     className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-[#383185] border-gray-300"
-                                    autoComplete=''
                                     required
                                 />
-                                <div onClick={togglePassword} className="absolute text-lg right-3 top-1/2 mt-[2px] transform -translate-y-1/2 text-gray-600 cursor-pointer" >
-                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </div>
+                            <div>
+                                <label className="block text-sm font-medium text-gray-600">Password</label>
+                                <div className='relative'>
+                                    <input
+                                        type={showPassword ? 'text' : 'password'}
+                                        {...register("password")}
+                                        placeholder="••••••••"
+                                        className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-[#383185] border-gray-300"
+                                        autoComplete=''
+                                        required
+                                    />
+                                    <div onClick={togglePassword} className="absolute text-lg right-3 top-1/2 mt-[2px] transform -translate-y-1/2 text-gray-600 cursor-pointer" >
+                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="text-right">
-                            <Link to="/forgot-password" className="text-sm text-[#383185] hover:underline underline-offset-5">
-                                Forgot Password?
-                            </Link>
-                        </div>
-                        <button
-                            type="submit"
-                            className="w-full bg-[#383185] hover:bg-[#363353] cursor-pointer text-white py-2 rounded-lg text-lg font-medium transition duration-200"
-                        >
-                            Login
-                        </button>
-                    </form>
-                </div>
+                            <div className="text-right">
+                                <Link to="/forgot-password" className="text-sm text-[#383185] hover:underline underline-offset-5">
+                                    Forgot Password?
+                                </Link>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full bg-[#383185] hover:bg-[#363353] cursor-pointer text-white py-2 rounded-lg text-lg font-medium transition duration-200"
+                            >
+                                Login
+                            </button>
+                        </form>
+                    </div>
 
-                {/* Home Page Link */}
-                <Link to="/" className='text-center text-[#383185] hover:underline underline-offset-5 mt-5 flex flex-wrap justify-center items-center'> <FaArrowLeft /> <span> Back to Home </span> </Link>
-            </div>
-        </Layout>
+                    {/* Home Page Link */}
+                    <Link to="/" className='text-center text-[#383185] hover:underline underline-offset-5 mt-5 flex flex-wrap justify-center items-center'> <FaArrowLeft /> <span> Back to Home </span> </Link>
+                </div>
+            </Layout>
+        </>
     )
 }
